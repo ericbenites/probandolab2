e614e81c5b64ea5b3a0d1b094353a9093eaf1b53
Eduardo Sanchez <a20122509@pucp.pe>
2020-04-26 17:38:25 -0500

    somosunoscsm

--- src/main/java/lab2/practicando/controllers/departmentsController.java
+++ src/main/java/lab2/practicando/controllers/departmentsController.java
@@ -1,4 +1,12 @@
 package lab2.practicando.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class departmentsController {
+    @GetMapping
+    public String primera(){
+        return "index";
+    }
 }
