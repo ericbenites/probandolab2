9824c231b6aa813e4a5e0cef7e55ebf80a1aa32b
lewis-coder <57689615+lewis-coder@users.noreply.github.com>
2020-04-26 17:02:42 -0500

    lewis2

--- src/main/java/lab2/practicando/entity/countries.java
+++ src/main/java/lab2/practicando/entity/countries.java
@@ -16,4 +16,28 @@ public class countries {
     private String country_name;
     @Column(name= "region_id")
     private BigDecimal region_id;
+
+    public String getCountry_id() {
+        return country_id;
+    }
+
+    public void setCountry_id(String country_id) {
+        this.country_id = country_id;
+    }
+
+    public String getCountry_name() {
+        return country_name;
+    }
+
+    public void setCountry_name(String country_name) {
+        this.country_name = country_name;
+    }
+
+    public BigDecimal getRegion_id() {
+        return region_id;
+    }
+
+    public void setRegion_id(BigDecimal region_id) {
+        this.region_id = region_id;
+    }
 }
